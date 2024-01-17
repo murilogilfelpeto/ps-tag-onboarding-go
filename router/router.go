af40df4c3d5b69a4f377430bce353bbce95e436a
Murilo Felpeto <murilo.gilfelpeto@wexinc.com>
2024-01-17 15:21:58 -0300

    refact: added a graceful shutdown

--- router/router.go
+++ router/router.go
@@ -1,9 +1,15 @@
 package router
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/murilogilfelpeto/ps-tag-onboarding-go/configuration"
 	"github.com/murilogilfelpeto/ps-tag-onboarding-go/handler"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var logger = configuration.NewLogger("router")
@@ -29,9 +35,31 @@ func (r *router) InitServer() {
 
 	r.initializeRoutes(router)
 
-	err := router.Run(":8080")
-	if err != nil {
-		logger.Errorf("Error starting server: %v", err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Errorf("Error starting server: %v", err)
+			panic(err)
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil && err != http.ErrServerClosed {
+		logger.Errorf("Error shutting down server: %v", err)
 		panic(err)
 	}
+
+	logger.Info("Server gracefully stopped")
 }
