d32ccc1ec64a72bfe18bef8052deea3c116c8030
Murilo Felpeto <murilo.gilfelpeto@wexinc.com>
2024-02-20 16:59:06 -0300

    test: split user entity mapper into separated files

--- internal/repository/mapper/userEntityMapper_test.go
+++ internal/repository/mapper/userEntityMapper_test.go
@@ -8,62 +8,40 @@ import (
 	"testing"
 )
 
-func TestUserMapping(t *testing.T) {
-	testCases := []struct {
-		TestName string
-		Entity   entities.UserEntity
-		Expected func(entity entities.UserEntity, t *testing.T)
-	}{
-		{
-			TestName: "When map user to user entity, then should return valid user entity",
-			Entity:   entities.UserEntity{},
-			Expected: func(entity entities.UserEntity, t *testing.T) {
-				user, _ := models.NewUser("", "John", "Doe", "john.doe@email.com", 36)
-				userEntity := UserToUserEntity(user)
-				assert.NotNil(t, userEntity.ID)
-				assert.Equal(t, user.GetFirstName(), userEntity.FirstName)
-				assert.Equal(t, user.GetLastName(), userEntity.LastName)
-				assert.Equal(t, user.GetEmail(), userEntity.Email)
-				assert.Equal(t, user.GetAge(), userEntity.Age)
-			},
-		},
-		{
-			TestName: "When map user entity to user, then should return valid user",
-			Entity: entities.UserEntity{
-				ID:        uuid.New(),
-				FirstName: "John",
-				LastName:  "Doe",
-				Email:     "john.doe@email.com",
-				Age:       36,
-			},
-			Expected: func(entity entities.UserEntity, t *testing.T) {
-				user, _ := UserEntityToUser(entity)
-				assert.Equal(t, user.GetID(), entity.ID.String())
-				assert.Equal(t, user.GetFirstName(), entity.FirstName)
-				assert.Equal(t, user.GetLastName(), entity.LastName)
-				assert.Equal(t, user.GetEmail(), entity.Email)
-				assert.Equal(t, user.GetAge(), entity.Age)
-			},
-		},
-		{
-			TestName: "When map user entity to user with invalid age, then should return error",
-			Entity: entities.UserEntity{
-				ID:        uuid.New(),
-				FirstName: "John",
-				LastName:  "Doe",
-				Email:     "john.doe@email.com",
-				Age:       17,
-			},
-			Expected: func(entity entities.UserEntity, t *testing.T) {
-				_, err := UserEntityToUser(entity)
-				assert.Error(t, err)
-			},
-		},
-	}
+func TestMapUserToUserEntity(t *testing.T) {
+	user, _ := models.NewUser("", "John", "Doe", "john.doe@email.com", 36)
+	userEntity := UserToUserEntity(user)
+	assert.NotNil(t, userEntity.ID)
+	assert.Equal(t, user.GetFirstName(), userEntity.FirstName)
+	assert.Equal(t, user.GetLastName(), userEntity.LastName)
+	assert.Equal(t, user.GetEmail(), userEntity.Email)
+	assert.Equal(t, user.GetAge(), userEntity.Age)
+}
 
-	for _, tc := range testCases {
-		t.Run(tc.TestName, func(t *testing.T) {
-			tc.Expected(tc.Entity, t)
-		})
+func TestMapUserEntityToUser(t *testing.T) {
+	userEntity := entities.UserEntity{
+		ID:        uuid.New(),
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "john.doe@email.com",
+		Age:       36,
 	}
+	user, _ := UserEntityToUser(userEntity)
+	assert.Equal(t, user.GetID(), userEntity.ID.String())
+	assert.Equal(t, user.GetFirstName(), userEntity.FirstName)
+	assert.Equal(t, user.GetLastName(), userEntity.LastName)
+	assert.Equal(t, user.GetEmail(), userEntity.Email)
+	assert.Equal(t, user.GetAge(), userEntity.Age)
+}
+
+func TestMapUserEntityToUser_WithInvalidData(t *testing.T) {
+	userEntity := entities.UserEntity{
+		ID:        uuid.New(),
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "john.doe@email.com",
+		Age:       17,
+	}
+	_, err := UserEntityToUser(userEntity)
+	assert.Error(t, err)
 }
